c266ea11293d3c7e5a2a2cc6fbe2f5f9e0081e51
Breno <brenocartoon@hotmail.com>
2024-02-11 21:31:16 -0300

    config: changing server configuration with a new structure Tag Routes returning a simple test for now, until the service creating

---
+++ src/main/routes/tag_routes.py
@@ -0,0 +1,9 @@
+from http import HTTPStatus
+from flask import Blueprint, request, jsonify
+
+tag_routes_bp = Blueprint('tag_routes', __name__)
+
+@tag_routes_bp.route('/create_tag', methods=['POST'])
+def create_tag():
+    print(request.json)
+    return jsonify({'response': 'ok'}), HTTPStatus.OK
